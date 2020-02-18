639dc11793ae2cbee3e141448008e3a77134680a
madetara <madetara@yandex.com>
2020-02-18 09:08:06 +0300

    chore(server): add prettier

--- api/server/.eslintrc.cjs
+++ api/server/.eslintrc.cjs
@@ -6,23 +6,26 @@ module.exports = {
         "sourceType": "module",
         "ecmaFeatures": {
             "impliedStrict": true
-        }   
+        }
     },
     plugins: [
         '@typescript-eslint',
+        "prettier",
         'node'
     ],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
-        'plugin:node/recommended'
+        'plugin:node/recommended',
+        "plugin:prettier/recommended"
     ],
     env: {
         "node": true,
         "es6": true
     },
     "rules": {
+        "prettier/prettier": "error",
         "node/no-missing-import": ["error", {
             "tryExtensions": [".js", ".json", ".node", ".ts"]
         }]
